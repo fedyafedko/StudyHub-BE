1fd849c7a8b4a635cb766e05d6d0344f13b34c57
Ivan Podhornyi <125029572+Podgore@users.noreply.github.com>
2024-01-24 22:14:08 +0200

    fix: make nullable refresh token (#69)

---
+++ StudyHub/StudyHub.DAL/Migrations/20240124165851_NullableRefreshTokenId.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudyHub.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableRefreshTokenId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_RefreshTokens_RefreshTokenId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RefreshTokenId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_RefreshTokens_RefreshTokenId",
+                table: "AspNetUsers",
+                column: "RefreshTokenId",
+                principalTable: "RefreshTokens",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_RefreshTokens_RefreshTokenId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "RefreshTokenId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_RefreshTokens_RefreshTokenId",
+                table: "AspNetUsers",
+                column: "RefreshTokenId",
+                principalTable: "RefreshTokens",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

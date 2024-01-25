cb0ec13d4bab4ae3a68323bd8c75d63bfdf9af6b
Федько Влад <vladfedko35@gmail.com>
2024-01-25 17:07:59 +0200

    fix: edit UserDTO

--- StudyHub/StudyHub.Common/DTO/User/UserDTO.cs
+++ StudyHub/StudyHub.Common/DTO/User/UserDTO.cs
@@ -4,8 +4,8 @@ public class UserDTO
 {
     public string FullName { get; set; } = string.Empty;
     public string Telegram { get; set; } = string.Empty;
-    public string Group { get; set; } = string.Empty;
-    public string Course { get; set; } = string.Empty;
-    public string Faculty { get; set; } = string.Empty;
+    public string? Group { get; set; } = null;
+    public string? Course { get; set; } = null;
+    public string? Faculty { get; set; } = null;
     public string Email { get; set; } = string.Empty;
 }
